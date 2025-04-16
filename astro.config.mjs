aa03b56eede1689d05fb6b1b08a76e789f5dfbef
Julian Pineda <918851+julianpinedayyz@users.noreply.github.com>
2025-04-16 17:11:08 -0500

    feat: add Heroicons and Astro React integration
    - Install @heroicons/react and @astrojs/react dependencies\n- Configure Astro React integration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
 
 export default defineConfig({
   site: 'https://julianpineda.com',
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   server: {
     host: '0.0.0.0',
     port: 3000
